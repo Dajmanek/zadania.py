d0a86fe67d1f44ea94204742f9ffed0fa0fb3d37
Dajmanek <dajmanxxx@gmail.com>
2020-12-11 21:02:21 +0100

    #

--- zadanie9.py
+++ zadanie9.py
@@ -2,12 +2,14 @@ try:
     readFile = open("plik.txt", "r")
     lines = readFile.readlines()
     readFile.close()
-except:
+except IOError as e:
+    print(e)
     print("Blad podczas odczytu pliku \"plik.txt\"")
     exit()
 try:
     writeFile = open("plik1.txt", "w")
     writeFile.writelines(lines)
     writeFile.close()
-except:
+except IOError as e:
+    print(e)
     print("Blad podczas zapisu pliku \"plik1.txt\"")
