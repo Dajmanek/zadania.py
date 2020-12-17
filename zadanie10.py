3c708cd69026db122f1549ff6355ff3ccf7bb622
Dajmanek <dajmanxxx@gmail.com>
2020-12-17 18:19:03 +0100

    #

--- zadanie10.py
+++ zadanie10.py
@@ -12,6 +12,11 @@ bridge_of_death = '''
 -Jaki jest twój ulubiony kolor?
 -Niebieski... nie... żóóóółtyyyy!'''
 
+# 1 rozwiązanie
 word_list = re.sub("[-.?!]", "", re.sub("\n", " ", bridge_of_death)).split(" ")
 filtered_word_list = list(filter(lambda element: len(element) >= 4, word_list))
 print(filtered_word_list)
+
+# 2 rozwiązanie
+pattern = re.compile(r'[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]{4,}')
+print(list(pattern.findall(bridge_of_death)))
