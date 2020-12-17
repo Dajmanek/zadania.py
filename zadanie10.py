0670c4f34db718170cbc98e33e628ddf739c2e2a
Dajmanek <dajmanxxx@gmail.com>
2020-12-17 16:36:38 +0100

    #

---
+++ zadanie10.py
@@ -0,0 +1,19 @@
+import re
+
+bridge_of_death = '''
+-Jaki jest twój ulubiony kolor?
+-Niebieski!
+-Dobrze. Idź.
+...
+-Stój. Jakie jest twe imię?
+-Sir Galahad z Camelotu.
+-Jaki jest twój cel?
+-Odnaleźć Graala.
+-Jaki jest twój ulubiony kolor?
+-Niebieski... nie... żóóóółtyyyy!'''
+
+word_list = re.sub("[-.?!]", "", re.sub("\n", " ", bridge_of_death)).split(" ")
+print(list(filter(lambda element: len(element) > 4, word_list)))
+
+
+
